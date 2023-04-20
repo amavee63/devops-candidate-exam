56162b2b43f7f3196b9855ec996455ce4eb202d5
Amar Veer <amar.veer.ext@siemens.com>
2023-04-20 16:17:53 +0530

    adding bucket in provider.tf

--- provider.tf
+++ provider.tf
@@ -3,8 +3,10 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
-'''
-S3 Bucket: "3.devops.candidate.exam"
-Region: "ap-south-1"
-Key: "amar.veer"
-'''
+terraform{
+	backend "s3" {
+		Bucket = "3.devops.candidate.exam"
+		Region = "ap-south-1"
+		Key    = "amar.veer" 
+	}
+}
